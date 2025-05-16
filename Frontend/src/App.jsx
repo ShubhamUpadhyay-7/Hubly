84c7d9ae6a555ae2e97620439a04d2251441dae7
Shubham Upadhyay <shubhamupadhyay8299@gmail.com>
2025-05-16 11:55:23 +0530

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from './Pages/LandingPage'
 import LoginPage from './Pages/LoginPage';
 import SignUp from './Pages/SignUp';
