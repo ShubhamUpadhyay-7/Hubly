8e42b18d5198bbe91a6b05295a2c1c4c11b7417b
Shubham Upadhyay <82092081+ShubhamUpadhyay-7@users.noreply.github.com>
2026-02-10 11:23:39 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,5 +6,6 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    console.log(import.meta.env.VITE_API_BASE_URL);
   </StrictMode>,
 )
